89f12511b8c8a7c084838ffd7df22c3c73119a7a
Graham Watson <graham@phantomwatson.com>
2018-09-12 20:06:54 -0400

    Move scatter plot backend code to table and fix point styling error

--- src/Model/Table/RelativeHomeValuesTable.php
+++ src/Model/Table/RelativeHomeValuesTable.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Model\Table;
 
+use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
+use Cake\Utility\Hash;
 use Cake\Validation\Validator;
 
 /**
@@ -134,5 +136,187 @@ class RelativeHomeValuesTable extends Table
             ->toArray();
 
         return $results['max'];
+    }
+
+    /**
+     * Returns an array of RelativeHomeValue values, keyed by county names
+     *
+     * @param bool $isNeighboring True if value is only for neighboring counties of each county
+     * @param string $type Either ratio or growth
+     * @return array
+     */
+    private function getResults($isNeighboring, $type)
+    {
+        $year = 2017;
+        $results = $this->find()
+            ->where([
+                'year' => $year,
+                'is_neighboring' => $isNeighboring,
+                "is_$type" => true
+            ])
+            ->select(['county_id', 'value'])
+            ->contain([
+                'Counties' => function (Query $q) {
+                    return $q->select(['id', 'name']);
+                }
+            ])
+            ->toArray();
+
+        return Hash::combine($results, '{n}.county.name', '{n}.value');
+    }
+
+    /**
+     * Returns the ideal/growth/warning/bad status of a given set of values
+     *
+     * @param float $growth Housing value growth value
+     * @param float $ratio County to state housing value ratio
+     * @return string
+     */
+    private function getStatus($growth, $ratio)
+    {
+        if ($growth <= $this->getStateGrowthValue()) {
+            return $ratio >= 1 ? 'warning' : 'bad';
+        }
+
+        return $ratio >= 1 ? 'ideal' : 'growth';
+    }
+
+    /**
+     * Returns the state average home value growth
+     *
+     * @return float
+     */
+    private function getStateGrowthValue()
+    {
+        return 0.084;
+    }
+
+    /**
+     * Returns the hex code for the color that a county with the given values should be displayed in
+     *
+     * @param float $growth Housing value growth value
+     * @param float $ratio County to state housing value ratio
+     * @return string
+     */
+    private function getColor($growth, $ratio)
+    {
+        $colors = $this->getColors();
+        $status = $this->getStatus($growth, $ratio);
+
+        return $colors[$status];
+    }
+
+    /**
+     * Returns an array of hex codes for colors used in scatter plot
+     *
+     * @return array
+     */
+    private function getColors()
+    {
+        return [
+            'ideal' => '#109618',
+            'growth' => '#3366cc',
+            'warning' => '#ff9900',
+            'bad' => '#dc3912'
+        ];
+    }
+
+    /**
+     * Returns an array of all variables needed to render the housing_barometer element
+     *
+     * @param null|int $countyId County ID, or null for all counties
+     * @return array
+     */
+    public function getVarsForScatterPlot($countyId = null)
+    {
+        // Relative home values
+        $rhvs = [];
+
+        foreach (['ratio', 'growth'] as $type) {
+            $rhvs['counties'][$type] = $this->getResults(false, $type);
+            $rhvs['neighboring'][$type] = $this->getResults(true, $type);
+        }
+
+        $chartData = [
+            [
+                'Growth value',
+
+                ['label' => 'Ideal', 'type' => 'number'],
+                ['type' => 'string', 'role' => 'tooltip'],
+                ['type' => 'string', 'role' => 'style'],
+
+                ['label' => 'Growth', 'type' => 'number'],
+                ['type' => 'string', 'role' => 'tooltip'],
+                ['type' => 'string', 'role' => 'style'],
+
+                ['label' => 'Warning', 'type' => 'number'],
+                ['type' => 'string', 'role' => 'tooltip'],
+                ['type' => 'string', 'role' => 'style'],
+
+                ['label' => 'Bad', 'type' => 'number'],
+                ['type' => 'string', 'role' => 'tooltip'],
+                ['type' => 'string', 'role' => 'style']
+            ],
+        ];
+        $countyNames = array_keys($rhvs['counties']['ratio']);
+        $columns = [
+            'ideal' => 1,
+            'growth' => 4,
+            'warning' => 7,
+            'bad' => 10
+        ];
+        foreach ($countyNames as $countyName) {
+            foreach (['counties', 'neighboring'] as $subject) {
+                $growth = $rhvs[$subject]['growth'][$countyName];
+                $ratio = $rhvs[$subject]['ratio'][$countyName];
+                $tooltip = $subject == 'counties'
+                    ? "$countyName County"
+                    : "Counties neighboring $countyName County";
+                $strokeColor = $this->getColor($growth, $ratio);
+                $fillColor = $subject == 'neighboring' ? '#ffffff' : $strokeColor;
+                $style = sprintf(
+                    'point {size: 4; shape-type: circle; stroke-color: %s; fill-color: %s;}',
+                    $strokeColor,
+                    $fillColor
+                );
+                $point = [
+                    $growth,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                ];
+
+                // Add values to columns corresponding to ideal/warning/etc. categories
+                $status = $this->getStatus($growth, $ratio);
+                $valueCol = $columns[$status];
+                $point[$valueCol] = (float)$ratio;
+                $tooltipCol = $valueCol + 1;
+                $point[$tooltipCol] = $tooltip;
+                $styleCol = $tooltipCol + 1;
+                $point[$styleCol] = $style;
+
+                $chartData[] = $point;
+            }
+        }
+
+        return [
+            'chartData' => $chartData,
+            'colors' => $this->getColors(),
+            'maxGrowth' => $this->getMaxValue('growth'),
+            'maxRatio' => $this->getMaxValue('ratio'),
+            'minGrowth' => $this->getMinValue('growth'),
+            'minRatio' => $this->getMinValue('ratio'),
+            'rhvs' => $rhvs,
+            'stateGrowthValue' => $this->getStateGrowthValue()
+        ];
     }
 }
